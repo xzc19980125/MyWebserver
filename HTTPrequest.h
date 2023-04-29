943dc0ecba1b3a4c61a4644b254a2f03d10575be
Shucicer <xzc0125@126.com>
2023-04-29 16:08:41 +0800

    增加注释

--- HTTPrequest.h
+++ HTTPrequest.h
@@ -19,22 +19,13 @@ public:
         BODY,
         FINISH,
     };
-
-    enum HTTP_CODE{
-        NO_REQUEST = 0,
-        GET_REQUEST,
-        BAD_REQUEST,
-        NO_RESOURSE,
-        FORBIDDENT_REQUEST,
-        FILE_REQUEST,
-        INTERNAL_ERROR,
-        CLOSED_CONNECTION,
-    };
     HTTPrequest(){init();};
     ~HTTPrequest()=default;
-
+    //初始化
     void init();
+    //解析请求
     bool parse(Buffer& buff);
+    //返回信息
     std::string path() const;
     std::string& path();
     std::string method() const;
@@ -55,12 +46,17 @@ private:
     static int convertHex(char ch);
     //解析状态(解析到什么位置了)
     PARSE_STATE state_;
+    //method_保存请求方式(GET/POST)
+    //path_保存请求中的路径(/mix/76.html?name=kelvin&password=123456)
+    //version_保存请求版本(HTTP/1.1中的1.1)
+    //body_保存请求体
     std::string method_,path_,version_,body_;
+    //保存请求头中的键值对
     std::unordered_map<std::string,std::string>header_;
+    //保存POST请求中的键值对
     std::unordered_map<std::string,std::string>post_;
-
+    //一些默认的HTML页面
     static const std::unordered_set<std::string>DEFAULT_HTML;
-
 
 };
 
